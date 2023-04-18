0b3ee1c451b82bb326fcca50e088ced95c7c0489
simonbiwer <91076673+simonbiwer@users.noreply.github.com>
2023-04-18 13:48:18 +0200

    setup vaadin project

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-@Theme(value = "mytodo")
+@Theme(value = "talent")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
