0c49456a919277e2ce0801216fef946e0d5aff39
Hendrik Oude Hengel <hendrik.oude@smail.inf.h-brs.de>
2023-04-19 11:56:24 +0200

    Improved main

--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -4,29 +4,41 @@ import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @PageTitle("Main")
 @Route(value = "")
-public class MainView extends HorizontalLayout {
+public class MainView extends VerticalLayout {
 
     private TextField name;
-    private Button sayHello;
+    private PasswordField password;
+    private Button login;
 
     public MainView() {
         name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
+        password = new PasswordField("Your password", "Enter Password here");
+        login = new Button("Login");
+
+        setWidthFull();
+        setHeightFull();
+
+        login.addClickListener(e -> {
+            Notification notification = Notification.show(name.getValue()+" is now logged in");
+            notification.setPosition(Notification.Position.MIDDLE);
+            name.setValue("");
+            password.setValue("");
         });
-        sayHello.addClickShortcut(Key.ENTER);
+        login.addClickShortcut(Key.ENTER);
 
         setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-
-        add(name, sayHello);
+        setHorizontalComponentAlignment(Alignment.CENTER, name, password, login);
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        add(name, password, login);
     }
 
 }
