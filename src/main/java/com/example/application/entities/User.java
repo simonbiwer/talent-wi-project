5e70d654ba9a487909c1c707b2d4f1ca99ea234d
eoegue2s <emirhan.oeguetcue@smail.inf.h-brs.de>
2023-05-02 13:31:21 +0200

    Kommentare für Klasse User hinzugefuegt

--- src/main/java/com/example/application/entities/User.java
+++ src/main/java/com/example/application/entities/User.java
@@ -1,20 +1,27 @@
 package com.example.application.entities;
 
 import jakarta.persistence.*;
-
+// import javax.persistence.*;
 import java.util.Objects;
-//import javax.persistence.*;
 import java.util.Collection;
+
+/**
+ * Implementation des User Entitätsklasse
+ * last edited: sb 02.05.23
+ */
 
 @Entity(name = "User")
 @Table(name="user", schema= "public")
 public class User {
+
+    // Id des Users, automatisch generiert
     @Id
     @SequenceGenerator(name = "User_id_use_seq")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_user")
     private int id_user;
 
+    // Getter und Setter für die ID
     public int getUserid() {
         return id_user;
     }
@@ -23,10 +30,12 @@ public class User {
         this.id_user = userid;
     }
 
+    // E-Mail des Users
     @Basic
     @Column(name = "email")
     private String email;
 
+    // Getter und Setter für die E-Mail
     public String getEmail() {
         return email;
     }
@@ -35,11 +44,12 @@ public class User {
         this.email = email;
     }
 
+    // Vorname des Users
     @Basic
     @Column(name = "vorname")
     private String vorname;
 
-
+    // Getter und Setter für den Vornamen
     public String getVorname() {
         return vorname;
     }
@@ -48,10 +58,12 @@ public class User {
         this.vorname = vorname;
     }
 
+    // Nachname des Users
     @Basic
     @Column(name = "nachname")
     private String nachname;
 
+    // Getter und Setter für den Nachnamen
     public String getNachname() {
         return nachname;
     }
@@ -60,10 +72,12 @@ public class User {
         this.nachname = nachname;
     }
 
+    // Passwort des Users
     @Basic
     @Column(name = "passwort")
     private String passwort;
 
+    // Getter und Setter für das Passwort
     public String getPasswort() {
         return passwort;
     }
