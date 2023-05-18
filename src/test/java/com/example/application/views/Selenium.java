d8b754eb3893781c868e2c63774d9e5440cfa479
Mervan M. Hella <merfan.halah@smail.inf.h-brs.de>
2023-05-18 18:29:56 +0200

    Selenium WebDriver test --> passed fuer Firefox, Edge aber Edge WebDriver fehlt

--- src/test/java/com/example/application/views/Selenium.java
+++ src/test/java/com/example/application/views/Selenium.java
@@ -1,5 +1,6 @@
 package com.example.application.views;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,6 +9,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -21,52 +25,75 @@ import static org.junit.jupiter.api.Assertions.*;
  * Selenium Test mit Selenuim WebDriver
  */
 
-class Selenium {
-    WebDriver driver;
-    // Launching the Chrome browser
+public class Selenium {
+    // WebDrivers für Chrome, Firefox und Edge
+    WebDriver chromeDriver;
+    WebDriver firefoxDriver;
+    WebDriver edgeDriver;
+
     @BeforeEach
-    void setUp() {
+    void setUpChrome() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver_win32/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("http://localhost:8080/login");
+        chromeDriver = new ChromeDriver();
     }
 
-    // Login Selenium Training and clicking on it
+    @BeforeEach
+    void setUpFirefox() {
+        System.setProperty("webdriver.firefox.driver", "src/test/resources/geckodriver_win32/geckodriver.exe");
+        firefoxDriver = new FirefoxDriver();
+    }
+
+//    @BeforeEach
+//    void setUpEdge() {
+//        System.setProperty("webdriver.edge.driver", "Pfad/zum/msedgedriver.exe");
+//        edgeDriver = new EdgeDriver();
+//    }
+
+    @AfterEach
+    void tearDownChrome() {
+        chromeDriver.quit();
+    }
+
+    @AfterEach
+    void tearDownFirefox() {
+        firefoxDriver.quit();
+    }
+//
+//    @AfterEach
+//    void tearDownEdge() {
+//        edgeDriver.quit();
+//    }
+
     @Test
-    public void login() throws InterruptedException {
-        Duration timeout = Duration.ofSeconds(5); // Set the timeout duration to 5 seconds
-        WebDriverWait wait = new WebDriverWait(driver , timeout); // Wait for a maximum of 5 seconds
-        WebElement loginField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("input-vaadin-email-field-6")));
-        driver.findElement(By.id("input-vaadin-email-field-6")).sendKeys("testtest@email.com");
-        driver.findElement(By.id("input-vaadin-password-field-7")).sendKeys("Test1234");
+    public void chromeAnmeldeTest() throws InterruptedException {
+        loginUndLogoutDurchfuehren(chromeDriver);
+    }
+
+    @Test
+    public void firefoxAufmeldeTest() throws InterruptedException {
+        loginUndLogoutDurchfuehren(firefoxDriver);
+    }
+
+//    @Test
+//    public void edgeAnmeldenTest() throws InterruptedException {
+//        loginUndLogoutDurchfuehren(edgeDriver);
+//    }
+
+    private void loginUndLogoutDurchfuehren(@NotNull WebDriver driver) throws InterruptedException {
+        driver.get("http://localhost:8080/login");
+        Duration timeout = Duration.ofSeconds(5);
+        WebDriverWait wait = new WebDriverWait(driver, timeout);
+        WebElement loginFeld = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("input-vaadin-email-field-6")));
+        driver.findElement(By.id("input-vaadin-email-field-6")).sendKeys("plz_dont_delete_this_user@sleniumtest.de");
+        driver.findElement(By.id("input-vaadin-password-field-7")).sendKeys("seleNiumtest1234");
         Thread.sleep(6000);
         driver.findElement(By.xpath("//*[@id=\"ROOT-2521314\"]/vaadin-vertical-layout/vaadin-horizontal-layout[2]/vaadin-vertical-layout/vaadin-button")).click();
         Thread.sleep(6000);
-        Assertions.assertEquals(driver.getCurrentUrl(), "http://localhost:8080/");
-    }
-//    //Logout
-//    @Test
-//    public void logout() throws InterruptedException {
-//        Duration timeout = Duration.ofSeconds(10); // Set the timeout duration to 10 seconds
-//        WebDriverWait wait = new WebDriverWait(driver , timeout); // Wait for a maximum of 10 seconds
-//        WebElement loginField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("input-vaadin-email-field-6")));
-//        driver.findElement(By.id("input-vaadin-email-field-6")).sendKeys("testtest@email.com");
-//        driver.findElement(By.id("input-vaadin-password-field-7")).sendKeys("Test1234");
-//        Thread.sleep(6000);
-//        driver.findElement(By.xpath("//*[@id=\"ROOT-2521314\"]/vaadin-vertical-layout/vaadin-horizontal-layout[2]/vaadin-vertical-layout/vaadin-button")).click();
-//        Thread.sleep(6000);
-//        wait = new WebDriverWait(driver , timeout);
-//        WebElement mainView = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("slot")));
-//        driver.findElement(By.id("slot")).click();
-//        Thread.sleep(6000);
+        Assertions.assertEquals("http://localhost:8080/", driver.getCurrentUrl());
 //        driver.findElement(By.xpath("//*[@id=\"ROOT-2521314\"]/vaadin-app-layout/vaadin-tabs/vaadin-tab[3]")).click();
 //        driver.findElement(By.xpath("//*[@id=\"ROOT-2521314\"]/vaadin-app-layout/vaadin-vertical-layout/vaadin-button")).click();
 //        Thread.sleep(6000);
 //        Assertions.assertEquals(driver.getCurrentUrl(), "http://localhost:8080/login/");
-//    }
 
-    @AfterEach
-    void tearDown() {
-        driver.quit();
     }
 }
